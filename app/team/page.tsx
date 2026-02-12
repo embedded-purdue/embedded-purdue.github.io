cca610b75de2f4c874dd0f383b623f465692b565
Gillian Hanley <gghanley04@gmail.com>
2026-02-12 14:51:52 -0500

    updates to padding

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -155,7 +155,7 @@ export default function TeamPage() {
 
       {/* Sections */}
       <section className="py-20 px-4">
-        <div className="max-w-7xl mx-auto p-14">
+        <div className="max-w-7xl mx-auto space-y-16 p-14">
           {sections.map((s) => {
             const Icon = s.icon;
             return (
